ac909097f257f74414e1d4c865e4ce57465044f1
d-otis <dan@dan-foley.com>
2020-08-21 14:33:27 -0400

    add routes for reservations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
       resources :items
+      resources :reservations, only: [:create, :update, :destroy, :show, :index]
     end
   end
 end
