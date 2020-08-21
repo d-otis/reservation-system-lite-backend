798f4a4f91295e1f43f210b6625e654d6a85321c
d-otis <dan@dan-foley.com>
2020-08-21 11:19:51 -0400

    upcase Item brands

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ num_items = 20
 
 num_items.times do
   Item.create(
-    title: Faker::Appliance.brand + " : " + Faker::Appliance.equipment,
+    title: Faker::Appliance.brand.upcase + " — " + Faker::Appliance.equipment,
     description: Faker::Lorem.paragraph(sentence_count: 5),
     serial_number: Faker::Device.serial,
     is_available: Faker::Boolean.boolean
