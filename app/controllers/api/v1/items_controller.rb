a556a884f7350d7a4e8e092c25b113ffc0b6543b
d-otis <dan@dan-foley.com>
2020-09-05 13:42:27 -0400

    sort Items by title in index view

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.all
+    items = Item.order(title: :asc)
 
     render json: ItemSerializer.new(items).serialized_json
   end
