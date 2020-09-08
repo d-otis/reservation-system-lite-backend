10fce41d5e38e158f60309bf0b120ea5ccebd06a
d-otis <dan@dan-foley.com>
2020-09-08 13:59:36 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_193641) do
+ActiveRecord::Schema.define(version: 2020_09_08_175803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 2020_09_05_193641) do
 
   create_table "reservations", force: :cascade do |t|
     t.text "notes"
-    t.boolean "in_progress"
-    t.boolean "is_complete"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
